4514e91e8ddbc3b1cb3cdb5b6affa27622eda4b5
LinKiN <869149689@qq.com>
2023-07-18 19:10:35 +0800

    学习逆向的一些代码

---
+++ Crackme0201/app/src/main/java/com/linkin/crackme0201/MainActivity.java
@@ -0,0 +1,84 @@
+package com.linkin.crackme0201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edit_userName;
+    private EditText edit_sn;
+    private Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("程序未注册");
+        edit_userName = (EditText) findViewById(R.id.edit_username);
+        edit_sn = (EditText) findViewById(R.id.edit_sn);
+        btn_register = (Button) findViewById(R.id.button_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!checkSN(edit_userName.getText().toString().trim(),
+                        edit_sn.getText().toString().trim())) {
+                    Toast.makeText(MainActivity.this,
+                            "无效用户名或注册码", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "恭喜您！注册成功", Toast.LENGTH_SHORT).show();
+                    btn_register.setEnabled(false);
+                    setTitle("程序已注册");
+                }
+            }
+        });
+    }
+
+    private boolean checkSN(String userName, String sn) {
+        try {
+            if ((userName == null) || (userName.length() == 0))
+                return false;
+            if ((sn == null) || (sn.length() != 16))
+                return false;
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.reset();
+            digest.update(userName.getBytes());
+            byte[] bytes = digest.digest();
+            String hexstr = toHexString(bytes, "");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hexstr.length(); i += 2) {
+                sb.append(hexstr.charAt(i));
+            }
+            String userSN = sb.toString();
+            //Log.d("crackme", hexstr);
+            //Log.d("crackme", userSN);
+            if (!userSN.equalsIgnoreCase(sn))
+                return false;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private static String toHexString(byte[] bytes, String separator) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xFF & b);
+            if(hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex).append(separator);
+        }
+        return hexString.toString();
+    }
+
+}
